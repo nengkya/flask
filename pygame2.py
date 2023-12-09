957830a15b4c3450fb0b8a5f9b99437c9b2b32d9
Hasna Dzakiyya Makin <nengkya@yahoo.com>
2023-12-09 17:46:22 +0700

    #delta_time must under while. If not, the frame is too quick.

--- pygame2.py
+++ pygame2.py
@@ -4,7 +4,6 @@ display = pygame.display.set_mode((1280, 750))
 clock = pygame.time.Clock()
 running = True
 player_position = pygame.Vector2(display.get_width() / 2, display.get_height() / 2)
-delta_time = 0
 
 while running:
     for event in pygame.event.get(): #must, so can get quit without Ctrl-C
@@ -31,9 +30,15 @@ while running:
         if player_position.x < 0:
             player_position.x = display.get_width()
         player_position.x -= 300 * delta_time
+
+    if key[pygame.K_d]:
+        if player_position.x > display.get_width():
+            player_position.x = 0
+        player_position.x += 300 * delta_time
         
     pygame.display.flip()
-
+    
+    #must under while. If not, the frame is too quick.
     delta_time = clock.tick(60) / 1000
 
 
